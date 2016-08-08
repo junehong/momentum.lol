656870e33a63062dc12344c1505a5f98c295477c
June Hong <june@scml-june.att.net>
2016-08-07 19:30:51 -0700

    updated main img and some style adjustment

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ var shiftPadding = 100; // This is the amount each slide moves up or down betwee
 
 var resizeTimeoutFn;
 $(document).ready(function(){
-//	MoveToSlide(1,2);
 	MoveToSlide(0,0);
 
 
