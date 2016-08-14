e6b9b234a139b5eb4aa33eb6af71a7a3d6ced0f8
June Hong <june@scml-june.att.net>
2016-08-13 20:34:54 -0700

    navigation underline in-out compfinished

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,23 @@
 
 $(document).ready(function(){
 	
-
+	$(".topMenu").mouseenter(function(){
+		console.log('mousen');
+		$(this).find('.bottom').css('width','0%').css('position','relative').css('float','left');
+		// $(this).find('.bottom').css('left','0px');
+		$(this).find('.bottom').stop().animate({
+			width : "100%"
+		}, 500, function(){}); 
+		//
+		//
+	});
+	$(".topMenu").mouseleave(function(){
+		console.log('leave');
+		$(this).find('.bottom').css('width','100%').css('position','relative').css('float','right');
+		$(this).find('.bottom').stop().animate({
+			width : "0%"
+		}, 500, function(){}); 
+	});
 });
+
+
