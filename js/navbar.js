0fb5d389515af635c373cd67279ddd7fd6be5455
June Hong <june@scml-june.att.net>
2016-08-13 22:03:53 -0700

    i like nothing

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,11 @@
 $(document).ready(function(){
 	
 	$(".topMenu").mouseenter(function(){
-		console.log('mousen');
+		if (parentSlideIndex == 1 || parentSlideIndex == 2) { // for "work" (parentslideindex == 1) and "contact" (2) slides, color the underline black since background will be white
+			$('.bottom').css('background-color','black');
+		} else {
+			$('.bottom').css('background-color','white'); // in the "about" (parentslideindex == 0) color the underline white
+		}
 		$(this).find('.bottom').css('width','0%').css('position','relative').css('float','left');
 		// $(this).find('.bottom').css('left','0px');
 		$(this).find('.bottom').stop().animate({
@@ -12,7 +16,7 @@ $(document).ready(function(){
 		//
 	});
 	$(".topMenu").mouseleave(function(){
-		console.log('leave');
+		
 		$(this).find('.bottom').css('width','100%').css('position','relative').css('float','right');
 		$(this).find('.bottom').stop().animate({
 			width : "0%"
